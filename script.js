55bee683bbffa61850c02538d6d12b248f09ccaa
MariamIdrees <thumininu94@gmail.com>
2026-08-10 21:07:20 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+let a= "bola"
+console.log (a)
+let b = 22;
+console.log (b)
+let age = 30;
+console.log (age)
+let beautiful = false;
+console.log (beautiful)
+let disability = null;
+console.log (disability)
+let maritalStatus;
+console.log (maritalStatus)
+
+let info = { name: "Bola", age: "20", phoneNumber: "0815738589454", }
+console.log (info)
+console.log(info.age)
+
+const fruits = ["apple", "banana", "pineapple",  "kiwi", "orange"  ]
+console.log (fruits)
+console.log (fruits.kiwi)
+
