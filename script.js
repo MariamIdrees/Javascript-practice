1ebd044167a7b1560492660b226ad5615ef041c6
MariamIdrees <thumininu94@gmail.com>
2026-08-11 22:14:07 +0100

    second commit

--- script.js
+++ script.js
@@ -19,3 +19,34 @@ const fruits = ["apple", "banana", "pineapple",  "kiwi", "orange"  ]
 console.log (fruits)
 console.log (fruits.kiwi)
 
+let statement = "This is my javascript practice"
+console.log (statement)
+
+console.log (statement.toUpperCase())
+
+let gmail = "MariamIdreeS@gmail.com"
+console.log (gmail)
+console.log (gmail.toLowerCase())
+
+let own = "    this is my computer        "
+console.log (own)
+console.log (own.trim())
+console.log (statement.length)
+console.log (statement.charAt(19))
+console.log (statement.indexOf("j"))
+console.log(statement.includes("practice"))
+console.log (own.slice(4))
+
+
+let phoneNumber = 40
+console.log (phoneNumber)
+console.log (phoneNumber.toString()+10 )
+console.log(phoneNumber.toFixed(2))
+console.log(phoneNumber.toExponential(3))
+console.log(parseInt(phoneNumber)+50)
+console.log(parseFloat(phoneNumber)+20)
+
+// let score = 83
+// console.log(Math.PI)
+// console.log(Math.SQRT1_2)
+// console.log(Math.ceil())
