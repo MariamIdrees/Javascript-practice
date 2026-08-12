531ed3db9f490fbbb207db2760b03547f3909438
MariamIdrees <thumininu94@gmail.com>
2026-08-12 22:15:54 +0100

    second commit

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ let a= "bola"
 console.log (a)
 let b = 22;
 console.log (b)
-let age = 30;
-console.log (age)
+// let age = 30;
+// console.log (age)
 let beautiful = false;
 console.log (beautiful)
 let disability = null;
@@ -46,7 +46,59 @@ console.log(phoneNumber.toExponential(3))
 console.log(parseInt(phoneNumber)+50)
 console.log(parseFloat(phoneNumber)+20)
 
-// let score = 83
-// console.log(Math.PI)
-// console.log(Math.SQRT1_2)
-// console.log(Math.ceil())
+let score = 83
+console.log(Math.PI)
+console.log(Math.SQRT1_2)
+console.log(Math.ceil(346.3))
+console.log(Math.min (23, 12, 5, 21, 85, 4))
+console.log(Math.max (34, 16, 89, 75, 90, 92, 64))
+console.log(Math.round (33.5))
+console.log (Math.random())
+
+let d = 53
+let e = 38
+
+console.log (d+e)
+console.log (d-e)
+console.log (d/e)
+console.log (d*e)
+console.log (d%e)
+
+console.log (d===e)
+console.log (d==e)
+console.log (d>e)
+console.log(d<e)
+console.log  (d>=e)
+console.log (d<=e)
+console.log (d!=e)
+
+let sandal = true
+let textbook = false
+let uniform = true
+
+console.log (uniform && textbook)
+console.log (uniform && textbook || sandal)
+console.log ( (!uniform))
+console.log ((!sandal))
+
+let i = 43
+let j = 24
+
+// console.log (i++)
+// console.log (i++)
+// console.log (i++)
+// console.log (i++)
+// console.log (i++)
+// console.log (i++)
+
+console.log (++i)
+
+console.log (--j)
+// console.log (j--)
+// console.log (j--)
+// console.log (j--)
+// console.log (j--)
+
+let age = 12
+let vote = (age >=18 ? "you can vote" : "you cannot vote")
+console.log (vote);
