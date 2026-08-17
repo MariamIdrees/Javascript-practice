6edd94be41edae5c76bb12e57700f0b8c2444246
MariamIdrees <thumininu94@gmail.com>
2026-08-17 23:00:59 +0100

    firfth commit

--- script.js
+++ script.js
@@ -1,88 +1,88 @@
-let a= "bola"
-console.log (a)
-let b = 22;
-console.log (b)
+// let a= "bola"
+// console.log (a)
+// let b = 22;
+// console.log (b)
 // let age = 30;
 // console.log (age)
-let beautiful = false;
-console.log (beautiful)
-let disability = null;
-console.log (disability)
-let maritalStatus;
-console.log (maritalStatus)
+// let beautiful = false;
+// console.log (beautiful)
+// let disability = null;
+// console.log (disability)
+// let maritalStatus;
+// console.log (maritalStatus)
 
-let info = { name: "Bola", age: "20", phoneNumber: "0815738589454", }
-console.log (info)
-console.log(info.age)
+// let info = { name: "Bola", age: "20", phoneNumber: "0815738589454", }
+// console.log (info)
+// console.log(info.age)
 
-const fruits = ["apple", "banana", "pineapple",  "kiwi", "orange"  ]
-console.log (fruits)
-console.log (fruits.kiwi)
+// const fruits = ["apple", "banana", "pineapple",  "kiwi", "orange"  ]
+// console.log (fruits)
+// console.log (fruits.kiwi)
 
-let statement = "This is my javascript practice"
-console.log (statement)
+// let statement = "This is my javascript practice"
+// console.log (statement)
 
-console.log (statement.toUpperCase())
+// console.log (statement.toUpperCase())
 
-let gmail = "MariamIdreeS@gmail.com"
-console.log (gmail)
-console.log (gmail.toLowerCase())
+// let gmail = "MariamIdreeS@gmail.com"
+// console.log (gmail)
+// // console.log (gmail.toLowerCase())
 
-let own = "    this is my computer        "
-console.log (own)
-console.log (own.trim())
-console.log (statement.length)
-console.log (statement.charAt(19))
-console.log (statement.indexOf("j"))
-console.log(statement.includes("practice"))
-console.log (own.slice(4))
+// let own = "    this is my computer        "
+// console.log (own)
+// console.log (own.trim())
+// console.log (statement.length)
+// console.log (statement.charAt(19))
+// console.log (statement.indexOf("j"))
+// console.log(statement.includes("practice"))
+// console.log (own.slice(4))
 
 
-let phoneNumber = 40
-console.log (phoneNumber)
-console.log (phoneNumber.toString()+10 )
-console.log(phoneNumber.toFixed(2))
-console.log(phoneNumber.toExponential(3))
-console.log(parseInt(phoneNumber)+50)
-console.log(parseFloat(phoneNumber)+20)
+// let phoneNumber = 40
+// console.log (phoneNumber)
+// console.log (phoneNumber.toString()+10 )
+// console.log(phoneNumber.toFixed(2))
+// console.log(phoneNumber.toExponential(3))
+// console.log(parseInt(phoneNumber)+50)
+// console.log(parseFloat(phoneNumber)+20)
 
-let score = 83
-console.log(Math.PI)
-console.log(Math.SQRT1_2)
-console.log(Math.ceil(346.3))
-console.log(Math.min (23, 12, 5, 21, 85, 4))
-console.log(Math.max (34, 16, 89, 75, 90, 92, 64))
-console.log(Math.round (33.5))
-console.log (Math.random())
+// let score = 83
+// console.log(Math.PI)
+// console.log(Math.SQRT1_2)
+// console.log(Math.ceil(346.3))
+// console.log(Math.min (23, 12, 5, 21, 85, 4))
+// console.log(Math.max (34, 16, 89, 75, 90, 92, 64))
+// console.log(Math.round (33.5))
+// console.log (Math.random())
 
-let d = 53
-let e = 38
+// let d = 53
+// let e = 38
 
-console.log (d+e)
-console.log (d-e)
-console.log (d/e)
-console.log (d*e)
-console.log (d%e)
+// console.log (d+e)
+// console.log (d-e)
+// console.log (d/e)
+// console.log (d*e)
+// console.log (d%e)
 
-console.log (d===e)
-console.log (d==e)
-console.log (d>e)
-console.log(d<e)
-console.log  (d>=e)
-console.log (d<=e)
-console.log (d!=e)
+// console.log (d===e)
+// console.log (d==e)
+// console.log (d>e)
+// console.log(d<e)
+// console.log  (d>=e)
+// console.log (d<=e)
+// console.log (d!=e)
 
-let sandal = true
-let textbook = false
-let uniform = true
+// let sandal = true
+// let textbook = false
+// let uniform = true
 
-console.log (uniform && textbook)
-console.log (uniform && textbook || sandal)
-console.log ( (!uniform))
-console.log ((!sandal))
+// console.log (uniform && textbook)
+// console.log (uniform && textbook || sandal)
+// console.log ( (!uniform))
+// console.log ((!sandal))
 
-let i = 43
-let j = 24
+// let i = 43
+// let j = 24
 
 // console.log (i++)
 // console.log (i++)
@@ -91,30 +91,58 @@ let j = 24
 // console.log (i++)
 // console.log (i++)
 
-console.log (++i)
+// console.log (++i)
 
-console.log (--j)
+// console.log (--j)
 // console.log (j--)
 // console.log (j--)
 // console.log (j--)
 // console.log (j--)
 
-let age = 12
-let vote = (age >=18 ? "you can vote" : "you cannot vote")
-console.log (vote);
+// let age = 12
+// let vote = (age >=18 ? "you can vote" : "you cannot vote")
+// console.log (vote);
 
- let k = parseInt(prompt ("Enter your first number"))
- let l = parseInt(prompt("Enter your second number"))
-alert(k+l)
+//  let k = parseInt(prompt ("Enter your first number"))
+//  let l = parseInt(prompt("Enter your second number"))
+// alert(k+l)
 
-alert ("Hello Everyone")
-let newAge = prompt("Enter your age")
-let newVote = newAge >=18 ? "you can vote" : "you cannot vote"
+// alert ("Hello Everyone")
+// let newAge = prompt("Enter your age")
+// let newVote = newAge >=18 ? "you can vote" : "you cannot vote"
 
-alert(newVote)
-console.log (typeof newAge)
-console.log (newAge)
+// alert(newVote)
+// console.log (typeof newAge)
+// console.log (newAge)
 
-let sCore = 45
-console.log ("this is the user ${sCore}")
+// let sCore = 45
+// console.log ("this is the user ${sCore}")
 
+// for (let m = 1; m <= 20; m++) {
+// if (m %2 ===1){
+//     console.log (m)
+// }
+// }
+
+// for (let n = 1; n <= 20; n++) {
+// console.log (n)
+// }
+
+// for (o=1; o<=20; o++){
+//     if (o%2===0){
+//         console.log (o)
+//     }
+// }
+
+// for (let p = 1; p <= 20; p++) {
+// if (p *2){
+//     console.log (p)
+// }
+// }
+for (q = 1; q <= 20; q++){
+    for (r = 1; r <=12; r++){
+        let s = `${q} * ${r} = ${q*r}`;
+    console.log (s);
+
+}
+}
